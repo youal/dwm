ddf107d65e1b9b77deed775163a247b54f3c78c6
John Doe <my@email.com>
2025-08-09 22:09:19 +0900

    Restores back default fonts

--- config.def.h
+++ config.def.h
@@ -9,8 +9,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "FiraCode Nerd Font:size=10" };
-static const char dmenufont[]       = "FiraCode Nerd:size=10";
+static const char *fonts[]          = { "monospace:size=20" };
+static const char dmenufont[]       = "monospace:size=20";
 static const char col_gray1[]       = "#222222"; /* background of tags */
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#dddddd"; /* numbers of the tags */
