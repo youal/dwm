fc73a83f00be67044ba4ea6385a418331ef3323b
Yassine Oualmakran <my@email.com>
2025-09-08 20:57:34 +0900

    Updates the size of the scratchpad

--- config.def.h
+++ config.def.h
@@ -77,7 +77,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "75x21", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "94x23", NULL };
 static const char *open_bookmarks_cmd[] = { "open_bookmarks" };
 static const char *start_firefox[] = { "firefox" };
 
